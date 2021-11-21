125cedc8e6c1231133a164eac2b0c98aa19a8056
Cuong Tran <amt.freelance@gmail.com>
2021-11-21 20:32:45 +0700

    Done 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0) {
+    if (a < b) {
+      let i = 0;
+      do {
+        sum = sum + a + i;
+        i += 1;
+      } while (i <= b - a);
+    } else if (a > b) {
+      let i = a - b;
+      do {
+        sum = sum + a - i;
+        i -= 1;
+      } while (i >= 0);
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
-
+sumAll(4, 1);
 // Do not edit below this line
 module.exports = sumAll;
